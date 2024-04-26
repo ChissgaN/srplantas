0a08c0645dbab0a2f503d473a37ee3aa29a4f357
ChissgaN <coderchissgan@gmail.com>
2024-04-26 14:52:09 -0600

    prueba react router

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { ShoppingCartProvider } from "./Components/ShoppingCartContext";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <ShoppingCartProvider>
           <Routes>
             <Route path="/" element={<Main />} />
