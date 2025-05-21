8c2e9bdf263eff5941d755e987b6280b50f433cd
ChissgaN <coderchissgan@gmail.com>
2025-05-20 22:31:26 -0600

    refactor Footer component structure for better readability

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,9 +6,14 @@ export default function Footer() {
     <>
       <div className="py-6 px-16 bg-gray-200 w-full font-merriweather ">
         <div className="flex justify-between items-center w-full">
-          <p className=" sm:text-[17] max-sm:text-[14px] ">
-            ©2025 Distribuidora Agricultura Especializada
-          </p>
+          <div>
+            <p className=" sm:text-[17] max-sm:text-[14px] ">
+              ©2025 Distribuidora Agricultura Especializada
+            </p>
+            <p className=" sm:text-[17] max-sm:text-[14px] ">
+              Todos los derechos reservados
+            </p>
+          </div>
           <img src={logo} alt="" className="w-28 " />
         </div>
       </div>
